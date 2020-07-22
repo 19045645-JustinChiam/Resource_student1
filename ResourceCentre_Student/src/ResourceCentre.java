283d797bb628b3fe5f63f5a2aac5a01e747e3ad9
19001627 <19001627@PFT-19001627.rp.edu.sg>
2020-07-22 16:42:20 +0800

    Sophia's Version

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -200,7 +200,6 @@ public class ResourceCentre {
 	
 	//================================= Option 3 Loan =================================
 	public static boolean doLoanCamcorder(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
-		//test1
 		boolean isLoaned = false;
 
 		for (int i = 0; i < camcorderList.size(); i++) {
@@ -227,16 +226,33 @@ public class ResourceCentre {
 			System.out.println("Camcorder " + tag + " loaned out");
 		}
 	}
-	
 	public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
-		// write your code here
-		return true;
-	}
-	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
-		// write your code here
-		
-		
-	}
+	    // write codes here
+	    
+	    boolean isLoaned = false;
+	    
+	    for (int i = 0; i < chromebookList.size(); i++) {
+	      if(tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag()) && chromebookList.get(i).getIsAvailable() == true) {
+	        chromebookList.get(i).setIsAvailable(false);
+	        chromebookList.get(i).setDueDate(dueDate);
+	        
+	        isLoaned = true;
+	      }
+	      
+	    }
+	    return isLoaned;
+	  }
+	  public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
+	    // write codes here
+	    String tag = Helper.readString("Enter asset tag > ");
+	    String due = Helper.readString("Enter due date > ");
+	    Boolean isLoaned =doLoanChromebook(chromebookList, tag, due);
+	    if (isLoaned == false) {
+	      System.out.println("Invalid asset tag");
+	    } else {
+	      System.out.println("Chromebook " + tag + " loaned out");
+	    }
+	  }
 	
 	//================================= Option 4 Return =================================
 	public static boolean doReturnCamcorder(ArrayList<Camcorder> camcorderList,String tag) {
